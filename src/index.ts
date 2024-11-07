dfd446c088cbb62551b7562a2aff1e7895e6a29e
Aotokitsuruya <contact@aotoki.me>
2024-11-07 21:31:19 +0800

    feat: basic template with minimal copy implemented

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log("Hello World");
+import fs from 'fs';
+import path from 'path';
+
+const projectName = "hello-world";
+const currentDir = process.cwd();
+const projectDir = path.join(currentDir, projectName);
+fs.mkdirSync(projectDir, { recursive: true });
+
+const templateName = "hello-world";
+const templateRoot = path.join(__dirname, '../templates');
+const templateDir = path.join(templateRoot, templateName);
+fs.cpSync(templateDir, projectDir, { recursive: true });
+
+console.log(`Project "${projectName}" created at ${projectDir}`);
