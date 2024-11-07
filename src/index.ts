704dd9c8334273d55b2c2a47f91a2614d01f72da
Aotokitsuruya <contact@aotoki.me>
2024-11-07 21:37:59 +0800

    feat: add prompts to ask for options

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-const projectName = "hello-world";
-const currentDir = process.cwd();
-const projectDir = path.join(currentDir, projectName);
-fs.mkdirSync(projectDir, { recursive: true });
+import prompts from "prompts";
 
-const templateName = "hello-world";
-const templateRoot = path.join(__dirname, '../templates');
-const templateDir = path.join(templateRoot, templateName);
-fs.cpSync(templateDir, projectDir, { recursive: true });
+async function init() {
+  const options = await prompts([
+    {
+      type: "text",
+      name: "name",
+      message: "Project name?",
+    },
+    {
+      type: "select",
+      name: "template",
+      message: "Choose a template",
+      choices: [{ title: "Hello World", value: "hello-world" }],
+    },
+  ]);
 
-console.log(`Project "${projectName}" created at ${projectDir}`);
+  const projectName = options.name;
+  const currentDir = process.cwd();
+  const projectDir = path.join(currentDir, projectName);
+  fs.mkdirSync(projectDir, { recursive: true });
+
+  const templateName = options.template;
+  const templateRoot = path.join(__dirname, "../templates");
+  const templateDir = path.join(templateRoot, templateName);
+  fs.cpSync(templateDir, projectDir, { recursive: true });
+
+  console.log(`Project "${projectName}" created at ${projectDir}`);
+}
+
+init().catch((e) => {
+  console.error(e);
+});
