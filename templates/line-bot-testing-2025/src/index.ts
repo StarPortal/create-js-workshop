e59090d5c9604b5f0945b89620dadd466de1d70f
Aotokitsuruya <contact@aotoki.me>
2024-11-18 14:21:48 +0800

    feat: replace embed tsyringe middleware to @hono/tsyringe

--- templates/line-bot-testing-2025/src/index.ts
+++ templates/line-bot-testing-2025/src/index.ts
@@ -4,7 +4,7 @@ import { Hono } from "hono";
 import { handle } from "@hono/node-server/vercel";
 import { webhook, messagingApi } from "@line/bot-sdk";
 
-import { dependencyInjection } from "@/app/tsyringe";
+import { tsyringe } from "@hono/tsyringe";
 import { verifySignature } from "@/app/lineBot";
 import { Config } from "@/app/config";
 import { MessagePresenter, TodoRepository } from "@/usecase/interface";
@@ -13,59 +13,59 @@ import { InMemoryTodoRepository } from "@/repository/inMemoryTodos";
 import { buildTodoCommandFromTextMessage } from "@/app/todoCommandFactory";
 
 export const config = {
-    api: {
-        bodyParser: false,
-    },
+  api: {
+    bodyParser: false,
+  },
 };
 
 container.register(Config, {
-    useFactory: () =>
-        new Config(
-            process.env.LINE_CHANNEL_ACCESS_TOKEN ?? "",
-            process.env.LINE_CHANNEL_SECRET ?? "",
-        ),
+  useFactory: () =>
+    new Config(
+      process.env.LINE_CHANNEL_ACCESS_TOKEN ?? "",
+      process.env.LINE_CHANNEL_SECRET ?? "",
+    ),
 });
 container.register<messagingApi.MessagingApiClient>(
-    messagingApi.MessagingApiClient,
-    {
-        useFactory: instanceCachingFactory((c) => {
-            const config = c.resolve(Config);
+  messagingApi.MessagingApiClient,
+  {
+    useFactory: instanceCachingFactory((c) => {
+      const config = c.resolve(Config);
 
-            return new messagingApi.MessagingApiClient({
-                channelAccessToken: config.channelAccessToken,
-            });
-        }),
-    },
+      return new messagingApi.MessagingApiClient({
+        channelAccessToken: config.channelAccessToken,
+      });
+    }),
+  },
 );
 container.register(MessagePresenter, { useToken: LineTodoPresenter });
 container.register(TodoRepository, { useToken: InMemoryTodoRepository });
 
 export const app = new Hono();
-app.use(dependencyInjection());
+app.use(tsyringe());
 
 app.post("/", verifySignature, async (c) => {
-    const body = await c.req.json();
-    const events = body.events as webhook.Event[];
+  const body = await c.req.json();
+  const events = body.events as webhook.Event[];
 
-    await Promise.all(
-        events.map(async (event) => {
-            if (event.type !== "message" || event.message.type !== "text") {
-                return null;
-            }
+  await Promise.all(
+    events.map(async (event) => {
+      if (event.type !== "message" || event.message.type !== "text") {
+        return null;
+      }
 
-            const { replyToken, message } = event;
-            const textMessage = {
-                replyToken: replyToken ?? "",
-                userId: event.source?.userId ?? "",
-                content: message.text,
-            };
+      const { replyToken, message } = event;
+      const textMessage = {
+        replyToken: replyToken ?? "",
+        userId: event.source?.userId ?? "",
+        content: message.text,
+      };
 
-            const usecase = buildTodoCommandFromTextMessage(c, textMessage);
-            await usecase.execute(textMessage);
-        }),
-    );
+      const usecase = buildTodoCommandFromTextMessage(c, textMessage);
+      await usecase.execute(textMessage);
+    }),
+  );
 
-    return c.text("OK");
+  return c.text("OK");
 });
 
 export default handle(app);
